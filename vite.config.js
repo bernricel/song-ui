993e7ed484422f382a7dd3b58e5e888989691f79
bernricel <brbmusngi.student@ua.edu.ph>
2026-04-23 20:44:36 +0800

    added allow host preview

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    allowedHosts: ["song-ui-mqll.onrender.com"], // for dev (optional)
     proxy: {
-      '/api': {
-        target: 'https://song-api-fohg.onrender.com',
+      "/api": {
+        target: "https://song-api-fohg.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+
+  preview: {
+    allowedHosts: ["song-ui-mqll.onrender.com"], // 🔥 REQUIRED for Render
+  },
+});
